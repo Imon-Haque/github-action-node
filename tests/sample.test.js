dd8ba0e4a2b6928b65ef133894fee0af42b9cba7
Imon Haque <azizulhaqueimon@gmail.com>
2022-03-10 01:14:06 +0600

    changed test case to fail

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,11 +1,11 @@
 describe('Sample Test', () => {
     it('should test that true === true', () => {
-        expect(true).toBe(true)
+        expect(true).toBe(true);
     });
 });
 
 describe('Sum Test', () => {
     it('should test that 3 + 5 === 8', () => {
-        expect(3+5).toBe(8)
+        expect(3+5).toBe(9);
     });
 });
