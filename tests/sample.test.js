2cde1ec552928cbfe1ada7a441a80966d3bfb9c8
Imon Haque <azizulhaqueimon@gmail.com>
2022-03-13 00:57:29 +0600

    adding environment variable to try out action with gihub secret

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -6,6 +6,12 @@ describe('Sample Test', () => {
 
 describe('Sum Test', () => {
     it('should test that 3 + 5 === 8', () => {
-        expect(3+5).toBe(9);
+        expect(3+5).toBe(8);
+    });
+});
+
+describe('Testing Environmental Variable', () => {
+    it('author name should be the value of env', () => {
+        expect(process.env.AUTHOR_NAME).toBe("IMON_HAQUE");
     });
 });
